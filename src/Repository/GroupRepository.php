ea399515391c89da1da6e0666e40a1e3b10d3e67
saltanovas <zumbass@gmail.com>
2021-05-07 02:43:40 +0300

    Entities' repositories

--- src/Repository/GroupRepository.php
+++ src/Repository/GroupRepository.php
@@ -3,48 +3,28 @@
 namespace App\Repository;
 
 use App\Entity\Group;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use App\Repository\Repository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @method Group|null find($id, $lockMode = null, $lockVersion = null)
- * @method Group|null findOneBy(array $criteria, array $orderBy = null)
- * @method Group[]    findAll()
- * @method Group[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
-class GroupRepository extends ServiceEntityRepository
+class GroupRepository extends Repository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Group::class);
+        parent::__contruct($registry, Group::class);
     }
 
-    // /**
-    //  * @return Group[] Returns an array of Group objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function removeUser($groupId, $userId)
     {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('g.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+        // if I use Group or User, symfony cannot find such classes
+        $group = $this->_em->find('App\Entity\Group', $groupId);
+        $user = $this->_em->find('App\Entity\User', $userId);
 
-    /*
-    public function findOneBySomeField($value): ?Group
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        $group->removeUser($user);
+        $user->removeGroup($group);
+
+        $this->insert($user);
+        $this->save();
+
+        //TODO: need some return to check whether remove was successful?
     }
-    */
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -3,48 +3,28 @@
 namespace App\Repository;
 
 use App\Entity\User;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use App\Repository\Repository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @method User|null find($id, $lockMode = null, $lockVersion = null)
- * @method User|null findOneBy(array $criteria, array $orderBy = null)
- * @method User[]    findAll()
- * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends Repository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
     }
 
-    // /**
-    //  * @return User[] Returns an array of User objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function removeGroupFromUser($userId, $groupId)
     {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+        // if I use Group or User, symfony cannot find such classes
+        $user = $this->_em->find('App\Entity\User', $userId);
+        $group = $this->_em->find('App\Entity\Group', $groupId);
 
-    /*
-    public function findOneBySomeField($value): ?User
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        $user->removeGroup($group);
+        $group->removeUser($user);
+
+        $this->insert($group);
+        $this->save();
+
+        //TODO: need some return to check whether remove was successful?
     }
-    */
 }
