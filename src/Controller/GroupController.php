8d88f0473b28ce3e851d1b987936428c3dd86ea7
saltanovas <zumbass@gmail.com>
2021-05-07 13:55:40 +0300

    Removed forms from controllers

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -2,27 +2,21 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Entity\Group;
+use App\Form\GroupFormType;
 use App\Repository\GroupRepository;
-use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class GroupController extends AbstractController
 {
     private GroupRepository $groupRepo;
-    private UserRepository $userRepo;
 
-    public function __construct(GroupRepository $groupRepo, UserRepository $userRepo)
+    public function __construct(GroupRepository $groupRepo)
     {
         $this->groupRepo = $groupRepo;
-        $this->userRepo = $userRepo;
     }
 
     /**
@@ -42,56 +36,10 @@ class GroupController extends AbstractController
      * @Method({"GET", "POST"})
      */
     public function create(Request $request)
-    {
-        $users = $this->userRepo->findBy(array(), array('name' => 'ASC'));
-
-        // hard coded form, saw is it possible to do the same thing in the twig
-        $form = $this->createFormBuilder(new Group())
-            ->add(
-                'name',
-                TextType::class,
-                [
-                    'label' => "Name:",
-                    'row_attr' => ['class' => 'form-group'],
-                    'attr' => ['class' => 'form-control']
-                ]
-            )
-            ->add(
-                'users',
-                EntityType::class,
-                [
-                    'label' => false,
-                    'row_attr' => ['class' => 'form-group'],
-                    'attr' =>
-                    [
-                        'class' => 'selectpicker form-control',
-                        'multiple' => null,
-                        'data-live-search' => "true",
-                        'title' => "Choose students to add",
-                        'data-selected-text-format' => 'count > 4',
-                        'data-count-selected-text' => '{0} users selected',
-                        'data-size' => '7'
-                    ],
-                    'class' => User::class,
-                    'required' => false,
-                    'by_reference' => false, //this one is important!!!
-                    'choices'  => $users,
-                    'multiple' => true,
-                    'choice_label' => 'name',
-                    'choice_value' => 'id'
-                ]
-            )
-            ->add(
-                'save',
-                SubmitType::class,
-                [
-                    'label' => 'Create',
-                    'attr' => ['class' => 'btn btn-primary mt-3']
-                ]
-            )
-            ->getForm();
-
+    {    
+        $form = $this->createForm(GroupFormType::class, new Group());
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $this->groupRepo->insert($form->getData());
             $this->groupRepo->save();
@@ -103,7 +51,26 @@ class GroupController extends AbstractController
     }
 
     /**
-     * @Route("/group/delete/{groupId}")
+     * @Route("/group/update/{groupId}", name="update_group")
+     * @Method({"GET", "POST"})
+     */
+    public function update(Request $request, $groupId)
+    {
+        $form = $this->createForm(GroupFormType::class, $this->groupRepo->getById($groupId));
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->groupRepo->insert($form->getData());
+            $this->groupRepo->save();
+
+            return $this->redirectToRoute('list_group');
+        }
+
+        return $this->render('group/create.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * @Route("/group/delete/{groupId}", name="delete_group")
      * @Method({"DELETE"})
      */
     public function deleteGroup($groupId)
@@ -123,70 +90,5 @@ class GroupController extends AbstractController
         $this->groupRepo->removeUser($groupId, $userId);
 
         return $this->redirectToRoute('list_group');
-    }
-
-    /**
-     * @Route("/group/update/{groupId}", name="create_group")
-     * @Method({"GET", "POST"})
-     */
-    public function update(Request $request, $groupId)
-    {
-        $users = $this->userRepo->findBy(array(), array('name' => 'ASC'));
-
-        // hard coded form, saw is it possible to do the same thing inside a twig
-        $form = $this->createFormBuilder($this->groupRepo->getById($groupId))
-            ->add(
-                'name',
-                TextType::class,
-                [
-                    'label' => "Name:",
-                    'row_attr' => ['class' => 'form-group'],
-                    'attr' => ['class' => 'form-control']
-                ]
-            )
-            ->add(
-                'users',
-                EntityType::class,
-                [
-                    'label' => false,
-                    'row_attr' => ['class' => 'form-group'],
-                    'attr' =>
-                    [
-                        'class' => 'selectpicker form-control',
-                        'multiple' => null,
-                        'data-live-search' => "true",
-                        'title' => "Choose students to add",
-                        'data-selected-text-format' => 'count > 4',
-                        'data-count-selected-text' => '{0} users selected',
-                        'data-size' => '7'
-                    ],
-                    'class' => User::class,
-                    'required' => false,
-                    'by_reference' => false, //this one is important!!!
-                    'choices'  => $users,
-                    'multiple' => true,
-                    'choice_label' => 'name',
-                    'choice_value' => 'id'
-                ]
-            )
-            ->add(
-                'save',
-                SubmitType::class,
-                [
-                    'label' => 'Create',
-                    'attr' => ['class' => 'btn btn-primary mt-3']
-                ]
-            )
-            ->getForm();
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->groupRepo->insert($form->getData());
-            $this->groupRepo->save();
-
-            return $this->redirectToRoute('list_group');
-        }
-
-        return $this->render('group/create.html.twig', array('form' => $form->createView()));
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,26 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Entity\Group;
-use App\Repository\GroupRepository;
+use App\Form\UserFormType;
 use App\Repository\UserRepository;
-use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     private UserRepository $userRepo;
-    private GroupRepository $groupRepo;
 
-    public function __construct(UserRepository $userRepo, GroupRepository $groupRepo)
+    public function __construct(UserRepository $userRepo)
     {
         $this->userRepo = $userRepo;
-        $this->groupRepo = $groupRepo;
     }
 
     /**
@@ -43,54 +37,28 @@ class UserController extends AbstractController
      */
     public function create(Request $request)
     {
-        $groups = $this->groupRepo->findBy(array(), array('name' => 'ASC'));
-
-        // hard coded form, saw is it possible to do the same thing inside a twig
-        $form = $this->createFormBuilder(new User())
-            ->add(
-                'name',
-                TextType::class,
-                [
-                    'label' => "Name:",
-                    'row_attr' => ['class' => 'form-group'],
-                    'attr' => ['class' => 'form-control']
-                ]
-            )
-            ->add(
-                'groups',
-                EntityType::class,
-                [
-                    'label' => false,
-                    'row_attr' => ['class' => 'form-group'],
-                    'attr' =>
-                    [
-                        'class' => 'selectpicker form-control',
-                        'multiple' => null,
-                        'data-live-search' => "true",
-                        'title' => "Choose groups to add student",
-                        'data-selected-text-format' => 'count > 4',
-                        'data-count-selected-text' => '{0} groups selected',
-                        'data-size' => '7'
-                    ],
-                    'class' => Group::class,
-                    'required' => false,
-                    'choices'  => $groups,
-                    'multiple' => true,
-                    'choice_label' => 'name',
-                    'choice_value' => 'id'
-                ]
-            )
-            ->add(
-                'save',
-                SubmitType::class,
-                [
-                    'label' => 'Create',
-                    'attr' => ['class' => 'btn btn-primary mt-3']
-                ]
-            )
-            ->getForm();
-
+        $form = $this->createForm(UserFormType::class, new User());
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->userRepo->insert($form->getData());
+            $this->userRepo->save();
+
+            return $this->redirectToRoute('list_user');
+        }
+
+        return $this->render('user/create.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * @Route("/user/update/{userId}")
+     * @Method({"GET", "POST"})
+     */
+    public function update(Request $request, $userId)
+    {
+        $form = $this->createForm(UserFormType::class, $this->userRepo->getById($userId));
+        $form->handleRequest($request);
+        
         if ($form->isSubmitted() && $form->isValid()) {
             $this->userRepo->insert($form->getData());
             $this->userRepo->save();
@@ -122,69 +90,5 @@ class UserController extends AbstractController
         $this->userRepo->removeGroupFromUser($userId, $groupId);
 
         return $this->redirectToRoute('list_user');
-    }
-
-    /**
-     * @Route("/user/update/{userId}")
-     * @Method({"GET", "POST"})
-     */
-    public function update(Request $request, $userId)
-    {
-        $groups = $this->groupRepo->findBy(array(), array('name' => 'ASC'));
-
-        // hard coded form, saw is it possible to do the same thing in the twig
-        $form = $this->createFormBuilder($this->userRepo->getById($userId))
-            ->add(
-                'name',
-                TextType::class,
-                [
-                    'label' => "Name:",
-                    'row_attr' => ['class' => 'form-group'],
-                    'attr' => ['class' => 'form-control']
-                ]
-            )
-            ->add(
-                'groups',
-                EntityType::class,
-                [
-                    'label' => false,
-                    'row_attr' => ['class' => 'form-group'],
-                    'attr' =>
-                    [
-                        'class' => 'selectpicker form-control',
-                        'multiple' => null,
-                        'data-live-search' => "true",
-                        'title' => "Choose groups to add student",
-                        'data-selected-text-format' => 'count > 4',
-                        'data-count-selected-text' => '{0} groups selected',
-                        'data-size' => '7'
-                    ],
-                    'class' => Group::class,
-                    'required' => false,
-                    'choices'  => $groups,
-                    'multiple' => true,
-                    'choice_label' => 'name',
-                    'choice_value' => 'id'
-                ]
-            )
-            ->add(
-                'save',
-                SubmitType::class,
-                [
-                    'label' => 'Create',
-                    'attr' => ['class' => 'btn btn-primary mt-3']
-                ]
-            )
-            ->getForm();
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->userRepo->insert($form->getData());
-            $this->userRepo->save();
-
-            return $this->redirectToRoute('list_user');
-        }
-
-        return $this->render('user/create.html.twig', array('form' => $form->createView()));
     }
 }
